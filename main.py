620e3190eecb49135751b6a1e0bc7fa6badd58fd
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-01-08 14:13:58 +0100

    add tool usage with a well defined system prompt

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from tools import available_functions
 
 
 def main():
@@ -25,7 +27,11 @@ def main():
     client = genai.Client(api_key=api_key)
     res = client.models.generate_content(
         model = "gemini-2.5-flash", 
-        contents = messages
+        contents = messages,
+        config = types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        ),
     )
     if not res.usage_metadata:
         raise RuntimeError("a problem occurred in the response")
@@ -36,6 +42,11 @@ def main():
     if args.verbose:
         print(f"Prompt tokens: {prompt_token_count}")
         print(f"Response tokens: {response_token_count}")
+    
+    if res.function_calls:
+        print("Function Calls :")
+        for function_call in res.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
     print("Response :")
     print(res.text)
 

a4a22478447e71e3dffa8b147daae8f37714a9fb
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-01-08 10:33:42 +0100

    add content generator

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent-boot!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY not found")
+
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print(f"User prompt: {content}")
+
+    client = genai.Client(api_key=api_key)
+    res = client.models.generate_content(
+        model = "gemini-2.5-flash", 
+        contents = content
+    )
+    if not res.usage_metadata:
+        raise RuntimeError("a problem occurred in the response")
+
+    prompt_token_count = res.usage_metadata.prompt_token_count
+    response_token_count = res.usage_metadata.candidates_token_count
+
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {response_token_count}")
+    print("Response :")
+    print(res.text)
 
 
 if __name__ == "__main__":
